f0dcc5a520b9d49e10e2de6c7b341bb3e5f88a8c
maru-256 <maru043@docomo.ne.jp>
2021-11-11 23:12:20 +0900

    add StringSetValue

--- src/main/java/net/kunmc/lab/value/StringSetValue.java
+++ src/main/java/net/kunmc/lab/value/StringSetValue.java
@@ -1,2 +1,73 @@
-package net.kunmc.lab.value;public class StringSetValue {
+package net.kunmc.lab.value;
+
+import dev.kotx.flylib.command.CommandContext;
+import dev.kotx.flylib.command.UsageBuilder;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StringSetValue extends SetValue<String> {
+    public StringSetValue(String... strings) {
+        this(new HashSet<>(Arrays.asList(strings)));
+    }
+
+    public StringSetValue(@NotNull Set<String> value) {
+        super(value);
+    }
+
+    @Override
+    public String invalidValueMessageOnAdd(String entryName, Set<String> element) {
+        return element.toArray(new String[0])[0] + "はすでに" + entryName + "に追加されています.";
+    }
+
+    @Override
+    public String succeedMessageOnAdd(String entryName, Set<String> element) {
+        return entryName + "に" + element.toArray(new String[0])[0] + "を追加しました.";
+    }
+
+    @Override
+    public String invalidValueMessageOnRemove(String entryName, Set<String> element) {
+        return element.toArray(new String[0])[0] + "は" + entryName + "に追加されていませんでした.";
+    }
+
+    @Override
+    public String succeedMessageOnRemove(String entryName, Set<String> element) {
+        return entryName + "から" + element.toArray(new String[0])[0] + "を削除しました.";
+    }
+
+    @Override
+    public String clearMessage(String entryName) {
+        return entryName + "をクリアしました.";
+    }
+
+    @Override
+    public void sendListMessage(CommandContext ctx, String entryName) {
+        String header = "-----" + entryName + "-----";
+        ctx.message(ChatColor.YELLOW + header);
+
+        ctx.success(this.stream().collect(Collectors.joining(",")));
+
+        ctx.message(ChatColor.YELLOW + StringUtils.repeat("-", header.length()));
+    }
+
+    @Override
+    public void appendArgument(UsageBuilder builder) {
+        builder.textArgument("StringArgument");
+    }
+
+    @Override
+    public boolean isCorrectArgument(Object argument) {
+        return true;
+    }
+
+    @Override
+    public Set<String> argumentToValue(Object argument) {
+        return Collections.singleton(argument.toString());
+    }
 }
