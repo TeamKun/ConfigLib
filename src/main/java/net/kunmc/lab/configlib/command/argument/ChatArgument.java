8681713fb8ba2cba70f48fc1ec5b7ec665c20cd4
maru-256 <maru043@docomo.ne.jp>
2021-11-17 11:09:03 +0900

    add ChatArgument

--- src/main/java/net/kunmc/lab/configlib/command/argument/ChatArgument.java
+++ src/main/java/net/kunmc/lab/configlib/command/argument/ChatArgument.java
@@ -1,2 +1,46 @@
-package net.kunmc.lab.configlib.command.argument;public class ChatArgument {
+package net.kunmc.lab.configlib.command.argument;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import dev.kotx.flylib.command.Argument;
+import dev.kotx.flylib.command.SuggestionAction;
+import net.minecraft.server.v1_16_R3.ArgumentChat;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ChatArgument implements Argument<String> {
+    private final String name;
+    private final SuggestionAction suggestionAction;
+
+    public ChatArgument(String name, @Nullable SuggestionAction suggestionAction) {
+        this.name = name;
+        this.suggestionAction = suggestionAction;
+    }
+
+    @Nullable
+    @Override
+    public com.mojang.brigadier.arguments.ArgumentType<?> getType() {
+        return ArgumentChat.a();
+    }
+
+    @Override
+    public String parse(@NotNull com.mojang.brigadier.context.CommandContext<net.minecraft.server.v1_16_R3.CommandListenerWrapper> commandContext, @NotNull String s) {
+        try {
+            return ArgumentChat.a(commandContext, s).getText();
+        } catch (CommandSyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Nullable
+    @Override
+    public SuggestionAction getSuggestion() {
+        return suggestionAction;
+    }
 }
