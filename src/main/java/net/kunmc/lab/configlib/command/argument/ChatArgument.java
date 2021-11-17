a715c0d80aada15f8e7435c3696faa01e18df4ad
maru-256 <maru043@docomo.ne.jp>
2021-11-17 11:02:47 +0900

    update CommandUtil

---
+++ src/main/java/net/kunmc/lab/configlib/command/argument/ChatArgument.java
@@ -0,0 +1,2 @@
+package net.kunmc.lab.configlib.command.argument;public class ChatArgument {
+}
--- src/main/java/net/kunmc/lab/configlib/util/CommandUtil.java
+++ src/main/java/net/kunmc/lab/configlib/util/CommandUtil.java
@@ -1,8 +1,11 @@
 package net.kunmc.lab.configlib.util;
 
+import dev.kotx.flylib.command.Argument;
 import dev.kotx.flylib.command.Command;
+import dev.kotx.flylib.command.UsageBuilder;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 public class CommandUtil {
     public static String getName(Command command) {
@@ -21,6 +24,17 @@ public class CommandUtil {
             Field field = Command.class.getDeclaredField("parent");
             field.setAccessible(true);
             return ((Command) field.get(command));
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static List<Argument<?>> getArguments(UsageBuilder builder) {
+        try {
+            Field field = UsageBuilder.class.getDeclaredField("arguments");
+            field.setAccessible(true);
+            return ((List<Argument<?>>) field.get(builder));
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
             return null;
