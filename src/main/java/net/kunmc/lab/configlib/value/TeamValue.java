afce23d72606e58a35e0f8083ce9cad20f8e9b98
maru-256 <maru043@docomo.ne.jp>
2022-01-11 08:13:57 +0900

    add TeamValue

---
+++ src/main/java/net/kunmc/lab/configlib/value/TeamValue.java
@@ -0,0 +1,136 @@
+package net.kunmc.lab.configlib.value;
+
+import dev.kotx.flylib.command.CommandContext;
+import dev.kotx.flylib.command.UsageBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
+/**
+ * If you need completions with NewScoreboard, you should register it with scoreboard(Scoreboard) method.
+ * When you use NewScoreboard, this class can't be deserialized. Therefore, a field must be qualified as transient at that time.
+ */
+public class TeamValue implements SingleValue<Team> {
+    private Team value;
+    private transient final Consumer<Team> consumer;
+    private transient Scoreboard scoreboard;
+    private transient boolean listable = true;
+    private transient boolean writable = true;
+
+    public TeamValue() {
+        this((Team) null);
+    }
+
+    public TeamValue(Team team) {
+        this(team, t -> {
+        });
+    }
+
+    public TeamValue(Consumer<Team> onSet) {
+        this(null, onSet);
+    }
+
+    public TeamValue(Team team, Consumer<Team> onSet) {
+        value = team;
+        consumer = onSet;
+        scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+    }
+
+    public @NotNull Scoreboard scoreboard() {
+        return scoreboard;
+    }
+
+    public TeamValue scoreboard(@NotNull Scoreboard scoreboard) {
+        this.scoreboard = scoreboard;
+        return this;
+    }
+
+    @Override
+    public boolean validateOnSet(Team newValue) {
+        return !value.getName().equals(newValue.getName());
+    }
+
+    @Override
+    public void onSetValue(Team newValue) {
+        consumer.accept(newValue);
+    }
+
+    public TeamValue writableByCommand(boolean writable) {
+        this.writable = writable;
+        return this;
+    }
+
+    @Override
+    public boolean writableByCommand() {
+        return writable;
+    }
+
+    @Override
+    public void appendArgument(UsageBuilder builder) {
+        builder.textArgument("TeamName", suggestionBuilder -> {
+            scoreboard.getTeams().stream()
+                    .map(Team::getName)
+                    .filter(s -> value == null || !s.equals(value.getName()))
+                    .forEach(suggestionBuilder::suggest);
+        });
+    }
+
+    @Override
+    public boolean isCorrectArgument(Object argument) {
+        return scoreboard.getTeams().stream()
+                .map(Team::getName)
+                .anyMatch(s -> s.equals(argument));
+    }
+
+    @Override
+    public Team argumentToValue(Object argument) {
+        return scoreboard.getTeam(argument.toString());
+    }
+
+    @Override
+    public Team value() {
+        return value;
+    }
+
+    @Override
+    public void value(Team value) {
+        this.value = value;
+    }
+
+    public TeamValue listable(boolean listable) {
+        this.listable = listable;
+        return this;
+    }
+
+    @Override
+    public boolean listable() {
+        return listable;
+    }
+
+    @Override
+    public String incorrectArgumentMessage(Object argument) {
+        return argument + "は存在しないチームです.";
+    }
+
+    @Override
+    public String invalidValueMessage(String entryName, Team newValue) {
+        return newValue.getName() + "はすでに設定されているチームです.";
+    }
+
+    @Override
+    public String succeedSetMessage(String entryName) {
+        return entryName + "の値を" + value.getName() + "に設定しました.";
+    }
+
+    @Override
+    public void sendListMessage(CommandContext ctx, String entryName) {
+        if (value == null) {
+            ctx.success(entryName + ": null");
+        } else {
+            ctx.success(entryName + ": " + value.getName());
+        }
+    }
+}
