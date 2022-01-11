73c6bb107c3da30a750aaab9d7e538da2a334b01
maru-256 <maru043@docomo.ne.jp>
2022-01-12 08:51:31 +0900

    add annotation to MaterialSetValue methods

--- src/main/java/net/kunmc/lab/configlib/value/MaterialSetValue.java
+++ src/main/java/net/kunmc/lab/configlib/value/MaterialSetValue.java
@@ -3,6 +3,7 @@ package net.kunmc.lab.configlib.value;
 import com.google.common.collect.Sets;
 import dev.kotx.flylib.command.CommandContext;
 import dev.kotx.flylib.command.UsageBuilder;
+import net.kunmc.lab.configlib.annotation.Internal;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -24,35 +25,41 @@ public class MaterialSetValue extends SetValue<Material> {
     }
 
     @Override
+    @Internal
     public String invalidValueMessageForAdd(String entryName, Set<Material> element) {
         Material m = element.toArray(new Material[0])[0];
         return m.name() + "はすでに" + entryName + "に追加されています.";
     }
 
     @Override
+    @Internal
     public String succeedMessageForAdd(String entryName, Set<Material> element) {
         Material m = element.toArray(new Material[0])[0];
         return entryName + "に" + m.name() + "を追加しました.";
     }
 
     @Override
+    @Internal
     public String invalidValueMessageForRemove(String entryName, Set<Material> element) {
         Material m = element.toArray(new Material[0])[0];
         return m.name() + "は" + entryName + "に追加されていませんでした.";
     }
 
     @Override
+    @Internal
     public String succeedMessageForRemove(String entryName, Set<Material> element) {
         Material m = element.toArray(new Material[0])[0];
         return entryName + "から" + m.name() + "を削除しました.";
     }
 
     @Override
+    @Internal
     public String clearMessage(String entryName) {
         return entryName + "をクリアしました.";
     }
 
     @Override
+    @Internal
     public void appendArgumentForAdd(UsageBuilder builder) {
         builder.textArgument("MaterialName", sb -> {
             Arrays.stream(Material.values())
@@ -64,6 +71,7 @@ public class MaterialSetValue extends SetValue<Material> {
     }
 
     @Override
+    @Internal
     public void appendArgumentForRemove(UsageBuilder builder) {
         builder.textArgument("MaterialName", sb -> {
             value.stream()
@@ -74,18 +82,21 @@ public class MaterialSetValue extends SetValue<Material> {
     }
 
     @Override
+    @Internal
     public boolean isCorrectArgumentForAdd(List<Object> argument, CommandSender sender) {
         return Arrays.stream(Material.values())
                 .anyMatch(m -> m.name().equals(argument.get(0).toString().toUpperCase()));
     }
 
     @Override
+    @Internal
     public boolean isCorrectArgumentForRemove(List<Object> argument, CommandSender sender) {
         return Arrays.stream(Material.values())
                 .anyMatch(m -> m.name().equals(argument.get(0).toString().toUpperCase()));
     }
 
     @Override
+    @Internal
     public Set<Material> argumentToValueForAdd(List<Object> argument, CommandSender sender) {
         return Arrays.stream(Material.values())
                 .filter(m -> m.name().equals(argument.get(0).toString().toUpperCase()))
@@ -93,6 +104,7 @@ public class MaterialSetValue extends SetValue<Material> {
     }
 
     @Override
+    @Internal
     public Set<Material> argumentToValueForRemove(List<Object> argument, CommandSender sender) {
         return Arrays.stream(Material.values())
                 .filter(m -> m.name().equals(argument.get(0).toString().toUpperCase()))
@@ -100,6 +112,7 @@ public class MaterialSetValue extends SetValue<Material> {
     }
 
     @Override
+    @Internal
     public void sendListMessage(CommandContext ctx, String entryName) {
         String header = "-----" + entryName + "-----";
         ctx.message(ChatColor.YELLOW + header);
