7d04086639bca1fddf062068654a8031e1901eb7
maru-256 <maru043@docomo.ne.jp>
2022-01-14 10:07:59 +0900

    add EnumSetValue

---
+++ src/main/java/net/kunmc/lab/configlib/value/EnumSetValue.java
@@ -0,0 +1,134 @@
+package net.kunmc.lab.configlib.value;
+
+import com.google.common.collect.Sets;
+import dev.kotx.flylib.command.CommandContext;
+import dev.kotx.flylib.command.UsageBuilder;
+import dev.kotx.flylib.command.arguments.StringArgument;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class EnumSetValue<T extends Enum<T>> extends SetValue<T> {
+    private final transient Class<?> enumClass;
+
+
+    public EnumSetValue(T... values) {
+        this(new HashSet<>(), values);
+    }
+
+    public EnumSetValue(@NotNull Set<T> value, T... values) {
+        super(value);
+        this.value.addAll(Sets.newHashSet(values));
+
+        enumClass = values.getClass().getComponentType();
+    }
+
+
+    private T[] constants() {
+        return ((T[]) enumClass.getEnumConstants());
+    }
+
+    @Override
+    protected void appendArgumentForAdd(UsageBuilder builder) {
+        builder.stringArgument("name", StringArgument.Type.WORD, sb -> {
+            Arrays.stream(constants())
+                    .map(Enum::name)
+                    .map(String::toLowerCase)
+                    .forEach(sb::suggest);
+        });
+    }
+
+    @Override
+    protected boolean isCorrectArgumentForAdd(List<Object> argument, CommandSender sender) {
+        return Arrays.stream(constants())
+                .anyMatch(m -> m.name().equals(argument.get(0).toString().toUpperCase()));
+    }
+
+    @Override
+    protected String incorrectArgumentMessageForAdd(List<Object> argument) {
+        return argument.get(0) + "は不明な値です.";
+    }
+
+    @Override
+    protected Set<T> argumentToValueForAdd(List<Object> argument, CommandSender sender) {
+        return Arrays.stream(constants())
+                .filter(m -> m.name().equals(argument.get(0).toString().toUpperCase()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    protected String invalidValueMessageForAdd(String entryName, Set<T> value) {
+        Enum<?> e = value.toArray(new Enum<?>[0])[0];
+        return e.name() + "はすでに" + entryName + "に追加されています.";
+    }
+
+    @Override
+    protected String succeedMessageForAdd(String entryName, Set<T> value) {
+        Enum<?> e = value.toArray(new Enum<?>[0])[0];
+        return entryName + "に" + e.name() + "を追加しました.";
+    }
+
+    @Override
+    protected void appendArgumentForRemove(UsageBuilder builder) {
+        builder.stringArgument("name", StringArgument.Type.WORD, sb -> {
+            value.stream()
+                    .map(Enum::name)
+                    .map(String::toLowerCase)
+                    .forEach(sb::suggest);
+        });
+    }
+
+    @Override
+    protected boolean isCorrectArgumentForRemove(List<Object> argument, CommandSender sender) {
+        return Arrays.stream(constants())
+                .anyMatch(x -> x.name().equals(argument.get(0).toString().toUpperCase()));
+    }
+
+    @Override
+    protected String incorrectArgumentMessageForRemove(List<Object> argument) {
+        return argument.get(0) + "は不明な値です.";
+    }
+
+    @Override
+    protected Set<T> argumentToValueForRemove(List<Object> argument, CommandSender sender) {
+        return Arrays.stream(constants())
+                .filter(x -> x.name().equals(argument.get(0).toString().toUpperCase()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    protected String invalidValueMessageForRemove(String entryName, Set<T> value) {
+        Enum<?> e = value.toArray(new Enum<?>[0])[0];
+        return e.name() + "は" + entryName + "に追加されていませんでした.";
+    }
+
+    @Override
+    protected String succeedMessageForRemove(String entryName, Set<T> value) {
+        Enum<?> e = value.toArray(new Enum<?>[0])[0];
+        return entryName + "から" + e.name() + "を削除しました.";
+    }
+
+    @Override
+    protected String clearMessage(String entryName) {
+        return entryName + "をクリアしました.";
+    }
+
+    @Override
+    protected void sendListMessage(CommandContext ctx, String entryName) {
+        String header = "-----" + entryName + "-----";
+        ctx.message(ChatColor.YELLOW + header);
+
+        ctx.success(value.stream()
+                .map(Enum::name)
+                .collect(Collectors.joining(",")));
+
+        ctx.message(ChatColor.YELLOW + StringUtils.repeat("-", header.length()));
+    }
+}
--- src/main/java/net/kunmc/lab/configlib/value/MaterialSetValue.java
+++ src/main/java/net/kunmc/lab/configlib/value/MaterialSetValue.java
@@ -1,124 +1,17 @@
 package net.kunmc.lab.configlib.value;
 
 import com.google.common.collect.Sets;
-import dev.kotx.flylib.command.CommandContext;
-import dev.kotx.flylib.command.UsageBuilder;
-import dev.kotx.flylib.command.arguments.StringArgument;
-import org.apache.commons.lang.StringUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-public class MaterialSetValue extends SetValue<Material> {
+public class MaterialSetValue extends EnumSetValue<Material> {
     public MaterialSetValue(Material... materials) {
         this(Sets.newHashSet(materials));
     }
 
     public MaterialSetValue(@NotNull Set<Material> value) {
         super(value);
-    }
-
-    @Override
-    protected void appendArgumentForAdd(UsageBuilder builder) {
-        builder.stringArgument("MaterialName", StringArgument.Type.WORD, sb -> {
-            Arrays.stream(Material.values())
-                    .filter(m -> !value.contains(m))
-                    .map(Material::name)
-                    .map(String::toLowerCase)
-                    .forEach(sb::suggest);
-        });
-    }
-
-    @Override
-    protected boolean isCorrectArgumentForAdd(List<Object> argument, CommandSender sender) {
-        return Arrays.stream(Material.values())
-                .anyMatch(m -> m.name().equals(argument.get(0).toString().toUpperCase()));
-    }
-
-    @Override
-    protected String incorrectArgumentMessageForAdd(List<Object> argument) {
-        return argument.get(0) + "は存在してないMaterialです.";
-    }
-
-    @Override
-    protected Set<Material> argumentToValueForAdd(List<Object> argument, CommandSender sender) {
-        return Arrays.stream(Material.values())
-                .filter(m -> m.name().equals(argument.get(0).toString().toUpperCase()))
-                .collect(Collectors.toSet());
-    }
-
-    @Override
-    protected String invalidValueMessageForAdd(String entryName, Set<Material> element) {
-        Material m = element.toArray(new Material[0])[0];
-        return m.name() + "はすでに" + entryName + "に追加されています.";
-    }
-
-    @Override
-    protected String succeedMessageForAdd(String entryName, Set<Material> element) {
-        Material m = element.toArray(new Material[0])[0];
-        return entryName + "に" + m.name() + "を追加しました.";
-    }
-
-    @Override
-    protected void appendArgumentForRemove(UsageBuilder builder) {
-        builder.stringArgument("MaterialName", StringArgument.Type.WORD, sb -> {
-            value.stream()
-                    .map(Material::name)
-                    .map(String::toLowerCase)
-                    .forEach(sb::suggest);
-        });
-    }
-
-    @Override
-    protected boolean isCorrectArgumentForRemove(List<Object> argument, CommandSender sender) {
-        return Arrays.stream(Material.values())
-                .anyMatch(m -> m.name().equals(argument.get(0).toString().toUpperCase()));
-    }
-
-    @Override
-    protected String incorrectArgumentMessageForRemove(List<Object> argument) {
-        return argument.get(0) + "は存在しないMaterialです.";
-    }
-
-    @Override
-    protected Set<Material> argumentToValueForRemove(List<Object> argument, CommandSender sender) {
-        return Arrays.stream(Material.values())
-                .filter(m -> m.name().equals(argument.get(0).toString().toUpperCase()))
-                .collect(Collectors.toSet());
-    }
-
-    @Override
-    protected String invalidValueMessageForRemove(String entryName, Set<Material> element) {
-        Material m = element.toArray(new Material[0])[0];
-        return m.name() + "は" + entryName + "に追加されていませんでした.";
-    }
-
-    @Override
-    protected String succeedMessageForRemove(String entryName, Set<Material> element) {
-        Material m = element.toArray(new Material[0])[0];
-        return entryName + "から" + m.name() + "を削除しました.";
-    }
-
-    @Override
-    protected String clearMessage(String entryName) {
-        return entryName + "をクリアしました.";
-    }
-
-    @Override
-    protected void sendListMessage(CommandContext ctx, String entryName) {
-        String header = "-----" + entryName + "-----";
-        ctx.message(ChatColor.YELLOW + header);
-
-        ctx.success(value.stream()
-                .map(Material::name)
-                .collect(Collectors.joining(",")));
-
-        ctx.message(ChatColor.YELLOW + StringUtils.repeat("-", header.length()));
     }
 }
