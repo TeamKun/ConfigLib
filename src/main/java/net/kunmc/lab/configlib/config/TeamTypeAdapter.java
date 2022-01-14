1cfff172842ab6d61650137c26bc2ce9aba8e5c7
maru-256 <maru043@docomo.ne.jp>
2022-01-14 10:36:09 +0900

    improve TeamTypeAdapter

--- src/main/java/net/kunmc/lab/configlib/config/TeamTypeAdapter.java
+++ src/main/java/net/kunmc/lab/configlib/config/TeamTypeAdapter.java
@@ -3,6 +3,8 @@ package net.kunmc.lab.configlib.config;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 import org.bukkit.Bukkit;
 import org.bukkit.scoreboard.Team;
 
@@ -21,9 +23,11 @@ public class TeamTypeAdapter extends TypeAdapter<Team> {
     @Override
     public Team read(JsonReader in) throws IOException {
         String s = in.nextString();
+
         Team t = Bukkit.getScoreboardManager().getMainScoreboard().getTeam(s);
         if (t == null) {
-            throw new IOException(String.format("Team %s is not found from MainScoreboard", s));
+            LogManager.getLogger().log(Level.WARN,
+                    String.format("Team [%s] is not found at MainScoreboard.", s));
         }
 
         return t;
