ae59c9f99ef0bb93614679fc977a21b64ec970bb
maru-256 <maru043@docomo.ne.jp>
2022-02-09 04:41:19 +0900

    add UUID2StringMapValue

---
+++ src/main/java/net/kunmc/lab/configlib/value/UUID2StringMapValue.java
@@ -0,0 +1,34 @@
+package net.kunmc.lab.configlib.value;
+
+import dev.kotx.flylib.command.UsageBuilder;
+import dev.kotx.flylib.command.arguments.StringArgument;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public class UUID2StringMapValue extends UUID2ObjectMapValue<String> {
+    @Override
+    protected void appendValueArgumentForPut(UsageBuilder builder) {
+        builder.stringArgument("string", StringArgument.Type.PHRASE);
+    }
+
+    @Override
+    protected boolean isCorrectValueArgumentForPut(List<Object> argument, CommandSender sender) {
+        return true;
+    }
+
+    @Override
+    protected String incorrectValueArgumentMessageForPut(List<Object> argument) {
+        return "";
+    }
+
+    @Override
+    protected String argumentToValueForPut(List<Object> argument, CommandSender sender) {
+        return ((String) argument.get(1));
+    }
+
+    @Override
+    protected String valueToString(String s) {
+        return s;
+    }
+}
