327b28faf91720d39aeed471632dfbfbc1a2c461
maru-256 <maru043@docomo.ne.jp>
2022-02-09 04:24:09 +0900

    add UUID2IntegerMapValue

---
+++ src/main/java/net/kunmc/lab/configlib/value/UUID2IntegerMapValue.java
@@ -0,0 +1,33 @@
+package net.kunmc.lab.configlib.value;
+
+import dev.kotx.flylib.command.UsageBuilder;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public class UUID2IntegerMapValue extends UUID2ObjectMapValue<Integer> {
+    @Override
+    protected void appendValueArgumentForPut(UsageBuilder builder) {
+        builder.integerArgument("integer");
+    }
+
+    @Override
+    protected boolean isCorrectValueArgumentForPut(List<Object> argument, CommandSender sender) {
+        return true;
+    }
+
+    @Override
+    protected String incorrectValueArgumentMessageForPut(List<Object> argument) {
+        return "";
+    }
+
+    @Override
+    protected Integer argumentToValueForPut(List<Object> argument, CommandSender sender) {
+        return ((Integer) argument.get(1));
+    }
+
+    @Override
+    protected String valueToString(Integer integer) {
+        return integer.toString();
+    }
+}
