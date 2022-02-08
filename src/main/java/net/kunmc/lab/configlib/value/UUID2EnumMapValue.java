d06b68b4ae5a0e6be35c80de5807824a78b99483
maru-256 <maru043@docomo.ne.jp>
2022-02-09 03:48:55 +0900

    UUID2EnumMapValue

---
+++ src/main/java/net/kunmc/lab/configlib/value/UUID2EnumMapValue.java
@@ -0,0 +1,59 @@
+package net.kunmc.lab.configlib.value;
+
+import dev.kotx.flylib.command.UsageBuilder;
+import dev.kotx.flylib.command.arguments.StringArgument;
+import org.bukkit.command.CommandSender;
+
+import java.util.*;
+
+public class UUID2EnumMapValue<T extends Enum<T>> extends UUID2ObjectMapValue<T> {
+    private transient final Class<T> clazz;
+
+    public UUID2EnumMapValue(T... t) {
+        this(new HashMap<>(), t);
+    }
+
+    public UUID2EnumMapValue(Map<UUID, T> map, T... t) {
+        super(map);
+
+        clazz = ((Class<T>) t.getClass().getComponentType());
+    }
+
+    private T[] constants() {
+        return clazz.getEnumConstants();
+    }
+
+    @Override
+    protected void appendValueArgumentForPut(UsageBuilder builder) {
+        builder.stringArgument("name", StringArgument.Type.WORD, sb -> {
+            Arrays.stream(constants())
+                    .map(Enum::name)
+                    .map(String::toLowerCase)
+                    .forEach(sb::suggest);
+        });
+    }
+
+    @Override
+    protected boolean isCorrectValueArgumentForPut(List<Object> argument, CommandSender sender) {
+        return Arrays.stream(constants())
+                .anyMatch(x -> x.name().equalsIgnoreCase(argument.get(1).toString()));
+    }
+
+    @Override
+    protected String incorrectValueArgumentMessageForPut(List<Object> argument) {
+        return argument.get(1) + "は不明な値です.";
+    }
+
+    @Override
+    protected T argumentToValueForPut(List<Object> argument, CommandSender sender) {
+        return Arrays.stream(constants())
+                .filter(x -> x.name().equalsIgnoreCase(argument.get(1).toString()))
+                .findFirst()
+                .get();
+    }
+
+    @Override
+    protected String valueToString(T t) {
+        return t.name();
+    }
+}
