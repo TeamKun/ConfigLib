caf77790b879940936c4852d83624331b12348d3
maru-256 <maru043@docomo.ne.jp>
2022-02-09 05:17:16 +0900

    Enum2DoubleMapValue

---
+++ src/main/java/net/kunmc/lab/configlib/value/Enum2DoubleMapValue.java
@@ -0,0 +1,43 @@
+package net.kunmc.lab.configlib.value;
+
+import dev.kotx.flylib.command.UsageBuilder;
+import org.bukkit.command.CommandSender;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Enum2DoubleMapValue<T extends Enum<T>> extends Enum2ObjectMapValue<T, Double> {
+    public Enum2DoubleMapValue(T... t) {
+        this(new HashMap<>(), t);
+    }
+
+    public Enum2DoubleMapValue(Map<T, Double> value, T... t) {
+        super(value, t);
+    }
+
+    @Override
+    protected void appendValueArgumentForPut(UsageBuilder builder) {
+        builder.doubleArgument("double");
+    }
+
+    @Override
+    protected boolean isCorrectValueArgumentForPut(List<Object> argument, CommandSender sender) {
+        return true;
+    }
+
+    @Override
+    protected String incorrectValueArgumentMessageForPut(List<Object> argument) {
+        return "";
+    }
+
+    @Override
+    protected Double argumentToValueForPut(List<Object> argument, CommandSender sender) {
+        return ((Double) argument.get(1));
+    }
+
+    @Override
+    protected String valueToString(Double d) {
+        return d.toString();
+    }
+}
