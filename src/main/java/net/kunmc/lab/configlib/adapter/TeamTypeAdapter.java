c210884cc683776a0fc0c42e44b9a4adfe096832
maru-256 <maru043@docomo.ne.jp>
2022-02-09 21:09:08 +0900

    add NullTeam

--- src/main/java/net/kunmc/lab/configlib/adapter/TeamTypeAdapter.java
+++ src/main/java/net/kunmc/lab/configlib/adapter/TeamTypeAdapter.java
@@ -3,6 +3,7 @@ package net.kunmc.lab.configlib.adapter;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import net.kunmc.lab.configlib.bukkit.NullTeam;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.bukkit.Bukkit;
@@ -28,6 +29,7 @@ public class TeamTypeAdapter extends TypeAdapter<Team> {
         if (t == null) {
             LogManager.getLogger().log(Level.WARN,
                     String.format("Team [%s] is not found at MainScoreboard.", s));
+            t = new NullTeam();
         }
 
         return t;
