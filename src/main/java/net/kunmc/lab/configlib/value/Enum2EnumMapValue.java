8854cf48a2d69c1c102afa985e8c205e559861d7
maru-256 <maru043@docomo.ne.jp>
2022-02-12 23:42:02 +0900

    add Enum2EnumMapValue

---
+++ src/main/java/net/kunmc/lab/configlib/value/Enum2EnumMapValue.java
@@ -0,0 +1,62 @@
+package net.kunmc.lab.configlib.value;
+
+import dev.kotx.flylib.command.UsageBuilder;
+import dev.kotx.flylib.command.arguments.StringArgument;
+import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Enum2EnumMapValue<K extends Enum<K>, V extends Enum<V>> extends Enum2ObjectMapValue<K, V, Enum2EnumMapValue<K, V>> {
+    private transient final Class<V> clazz;
+
+    public Enum2EnumMapValue(Class<V> valueClass, K... k) {
+        this(new HashMap<>(), valueClass, k);
+    }
+
+    public Enum2EnumMapValue(Map<K, V> value, Class<V> valueClass, K... k) {
+        super(value, k);
+
+        this.clazz = valueClass;
+    }
+
+    private V[] constants() {
+        return clazz.getEnumConstants();
+    }
+
+    @Override
+    protected void appendValueArgumentForPut(UsageBuilder builder) {
+        builder.stringArgument("value", StringArgument.Type.WORD, sb -> {
+            Arrays.stream(constants())
+                    .map(Enum::name)
+                    .map(String::toLowerCase)
+                    .forEach(sb::suggest);
+        });
+    }
+
+    @Override
+    protected boolean isCorrectValueArgumentForPut(List<Object> argument, CommandSender sender) {
+        return Arrays.stream(constants())
+                .anyMatch(x -> x.name().equalsIgnoreCase(argument.get(1).toString()));
+    }
+
+    @Override
+    protected String incorrectValueArgumentMessageForPut(List<Object> argument) {
+        return argument.get(1) + "は不明な値です.";
+    }
+
+    @Override
+    protected V argumentToValueForPut(List<Object> argument, CommandSender sender) {
+        return Arrays.stream(constants())
+                .filter(x -> x.name().equalsIgnoreCase(argument.get(1).toString()))
+                .findFirst()
+                .get();
+    }
+
+    @Override
+    protected String valueToString(V v) {
+        return v.name();
+    }
+}
