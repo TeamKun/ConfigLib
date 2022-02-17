f010d05bfe1cfa20bf88de7c7feb0bdbcce14f84
maru-256 <maru043@docomo.ne.jp>
2022-02-18 01:11:14 +0900

    improve ConfigModifyCommand

--- src/main/java/net/kunmc/lab/configlib/ConfigModifyCommand.java
+++ src/main/java/net/kunmc/lab/configlib/ConfigModifyCommand.java
@@ -32,64 +32,81 @@ class ConfigModifyCommand extends AccessibleCommand {
 
     private void init(BaseConfig config, AccessibleCommand command) {
         for (Field field : ConfigUtil.getSingleValueFields(config)) {
+            SingleValue<?, ?> v;
+            try {
+                v = ((SingleValue<?, ?>) field.get(config));
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            if (!v.writableByCommand()) {
+                continue;
+            }
+
             command.appendChild(new Command(field.getName()) {
                 {
-                    try {
-                        SingleValue<?, ?> v = ((SingleValue<?, ?>) field.get(config));
-                        if (v.writableByCommand()) {
-                            children(new ModifySetCommand(field, v, config));
+                    children(new ModifySetCommand(field, v, config));
 
-                            if (v instanceof NumericValue) {
-                                children(new ModifyIncCommand(field, ((NumericValue<?, ?>) v), config));
-                                children(new ModifyDecCommand(field, ((NumericValue<?, ?>) v), config));
-                            }
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    if (v instanceof NumericValue) {
+                        children(new ModifyIncCommand(field, ((NumericValue<?, ?>) v), config));
+                        children(new ModifyDecCommand(field, ((NumericValue<?, ?>) v), config));
                     }
                 }
             });
         }
 
         for (Field field : ConfigUtil.getCollectionValueFields(config)) {
+            CollectionValue<?, ?, ?> v;
+            try {
+                v = ((CollectionValue<?, ?, ?>) field.get(config));
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            if (!v.addableByCommand() && !v.removableByCommand() && !v.clearableByCommand()) {
+                continue;
+            }
+
             command.appendChild(new Command(field.getName()) {
                 {
-                    try {
-                        CollectionValue value = ((CollectionValue) field.get(config));
-
-                        if (value.addableByCommand()) {
-                            children(new ModifyAddCommand(field, value, config));
-                        }
-                        if (value.removableByCommand()) {
-                            children(new ModifyRemoveCommand(field, value, config));
-                        }
-                        if (value.clearableByCommand()) {
-                            children(new ModifyClearCommand(field, value, config));
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    if (v.addableByCommand()) {
+                        children(new ModifyAddCommand(field, v, config));
+                    }
+                    if (v.removableByCommand()) {
+                        children(new ModifyRemoveCommand(field, v, config));
+                    }
+                    if (v.clearableByCommand()) {
+                        children(new ModifyClearCommand(field, v, config));
                     }
                 }
             });
         }
 
         for (Field field : ConfigUtil.getMapValueFields(config)) {
+            MapValue<?, ?, ?> v;
+            try {
+                v = ((MapValue<?, ?, ?>) field.get(config));
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            if (!v.puttableByCommand() && !v.removableByCommand() && !v.clearableByCommand()) {
+                continue;
+            }
+
             command.appendChild(new Command(field.getName()) {
                 {
-                    try {
-                        MapValue value = ((MapValue) field.get(config));
-
-                        if (value.puttableByCommand()) {
-                            children(new ModifyMapPutCommand(field, value, config));
-                        }
-                        if (value.removableByCommand()) {
-                            children(new ModifyMapRemoveCommand(field, value, config));
-                        }
-                        if (value.clearableByCommand()) {
-                            children(new ModifyMapClearCommand(field, value, config));
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    if (v.puttableByCommand()) {
+                        children(new ModifyMapPutCommand(field, v, config));
+                    }
+                    if (v.removableByCommand()) {
+                        children(new ModifyMapRemoveCommand(field, v, config));
+                    }
+                    if (v.clearableByCommand()) {
+                        children(new ModifyMapClearCommand(field, v, config));
                     }
                 }
             });
