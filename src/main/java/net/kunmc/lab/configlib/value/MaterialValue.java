2d4872b6a4dcee7ed28c282bc30a33e91aa4fba2
maru-256 <maru043@docomo.ne.jp>
2022-02-23 02:06:36 +0900

    add MaterialValue constructor

--- src/main/java/net/kunmc/lab/configlib/value/MaterialValue.java
+++ src/main/java/net/kunmc/lab/configlib/value/MaterialValue.java
@@ -2,8 +2,17 @@ package net.kunmc.lab.configlib.value;
 
 import org.bukkit.Material;
 
+import java.util.Arrays;
+
 public class MaterialValue extends EnumValue<Material> {
     public MaterialValue(Material value) {
         super(value);
+    }
+
+    public MaterialValue(Material value, boolean onlyBlock) {
+        super(value, Arrays.stream(Material.values())
+                .filter(x -> !onlyBlock || x.isBlock())
+                .toArray(Material[]::new)
+        );
     }
 }
