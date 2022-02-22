3d1cf42a0fb2b0c91143c0a1c2bc97ab5b0a3ba3
maru-256 <maru043@docomo.ne.jp>
2022-02-23 01:32:46 +0900

    improve Team2BlockDataMapValue

--- src/main/java/net/kunmc/lab/configlib/value/Team2BlockDataMapValue.java
+++ src/main/java/net/kunmc/lab/configlib/value/Team2BlockDataMapValue.java
@@ -1,49 +1,47 @@
 package net.kunmc.lab.configlib.value;
 
 import dev.kotx.flylib.command.UsageBuilder;
-import dev.kotx.flylib.command.arguments.StringArgument;
-import org.bukkit.Material;
+import net.kunmc.lab.configlib.argument.TileArgument;
+import net.kunmc.lab.configlib.util.CommandUtil;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.command.CommandSender;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Team2BlockDataMapValue extends Team2ObjectMapValue<BlockData, Team2BlockDataMapValue> {
+    private transient boolean listOnlyBlockName = false;
+
+    public Team2BlockDataMapValue listOnlyBlockName(boolean listOnlyBlockName) {
+        this.listOnlyBlockName = listOnlyBlockName;
+        return this;
+    }
+
     @Override
     protected void appendValueArgumentForPut(UsageBuilder builder) {
-        builder.stringArgument("name", StringArgument.Type.WORD, sb -> {
-            Arrays.stream(Material.values())
-                    .filter(Material::isBlock)
-                    .map(Material::name)
-                    .map(String::toLowerCase)
-                    .forEach(sb::suggest);
-        });
+        CommandUtil.addArgument(builder, new TileArgument("name"));
     }
 
     @Override
     protected boolean isCorrectValueArgumentForPut(List<Object> argument, CommandSender sender) {
-        return Arrays.stream(Material.values())
-                .filter(Material::isBlock)
-                .anyMatch(m -> m.name().equalsIgnoreCase(argument.get(1).toString()));
+        return true;
     }
 
     @Override
     protected String incorrectValueArgumentMessageForPut(List<Object> argument) {
-        return argument.get(1) + "はブロック化出来ない値です.";
+        return "";
     }
 
     @Override
     protected BlockData argumentToValueForPut(List<Object> argument, CommandSender sender) {
-        return Arrays.stream(Material.values())
-                .filter(m -> m.name().equalsIgnoreCase(argument.get(1).toString()))
-                .map(Material::createBlockData)
-                .findFirst()
-                .get();
+        return ((BlockData) argument.get(1));
     }
 
     @Override
     protected String valueToString(BlockData blockData) {
-        return blockData.getMaterial().name();
+        if (listOnlyBlockName) {
+            return blockData.getMaterial().name();
+        } else {
+            return blockData.getAsString();
+        }
     }
 }
