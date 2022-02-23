0fe3252e2c4ce294bb560b30a01469e212e07dcf
maru-256 <maru043@docomo.ne.jp>
2022-02-23 23:45:36 +0900

    add String2IntegerMapValue

---
+++ src/main/java/net/kunmc/lab/configlib/value/map/String2IntegerMapValue.java
@@ -0,0 +1,43 @@
+package net.kunmc.lab.configlib.value.map;
+
+import dev.kotx.flylib.command.UsageBuilder;
+import org.bukkit.command.CommandSender;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class String2IntegerMapValue extends String2ObjectMapValue<Integer, String2IntegerMapValue> {
+    public String2IntegerMapValue() {
+        this(new HashMap<>());
+    }
+
+    public String2IntegerMapValue(Map<String, Integer> value) {
+        super(value);
+    }
+
+    @Override
+    protected void appendValueArgumentForPut(UsageBuilder builder) {
+        builder.integerArgument("integer");
+    }
+
+    @Override
+    protected boolean isCorrectValueArgumentForPut(List<Object> argument, CommandSender sender) {
+        return true;
+    }
+
+    @Override
+    protected String incorrectValueArgumentMessageForPut(List<Object> argument) {
+        return "";
+    }
+
+    @Override
+    protected Integer argumentToValueForPut(List<Object> argument, CommandSender sender) {
+        return ((Integer) argument.get(1));
+    }
+
+    @Override
+    protected String valueToString(Integer integer) {
+        return integer.toString();
+    }
+}
