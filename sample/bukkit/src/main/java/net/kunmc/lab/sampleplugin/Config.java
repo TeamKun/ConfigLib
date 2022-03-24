e0785fb57bfccb3c148ee84a75002fa5d69bcfd9
Maru32768 <maru043@docomo.ne.jp>
2022-03-24 14:36:47 +0900

    add bukkit sample

---
+++ sample/bukkit/src/main/java/net/kunmc/lab/sampleplugin/Config.java
@@ -0,0 +1,21 @@
+package net.kunmc.lab.sampleplugin;
+
+import net.kunmc.lab.configlib.BaseConfig;
+import net.kunmc.lab.configlib.value.IntegerValue;
+import net.kunmc.lab.configlib.value.LocationValue;
+import net.kunmc.lab.configlib.value.collection.BlockDataSetValue;
+import net.kunmc.lab.configlib.value.map.Enum2DoubleMapValue;
+import org.bukkit.Material;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class Config extends BaseConfig {
+    public final IntegerValue integerValue = new IntegerValue(123);
+    public final LocationValue locationValue = new LocationValue();
+    public final BlockDataSetValue blockDataSetValue = new BlockDataSetValue();
+    public final Enum2DoubleMapValue<Material> enum2DoubleMapValue = new Enum2DoubleMapValue<>();
+
+    public Config(@NotNull Plugin plugin) {
+        super(plugin);
+    }
+}
---
+++ sample/bukkit/src/main/java/net/kunmc/lab/sampleplugin/SamplePlugin.java
@@ -0,0 +1,21 @@
+package net.kunmc.lab.sampleplugin;
+
+import net.kunmc.lab.commandlib.CommandLib;
+import net.kunmc.lab.configlib.ConfigCommand;
+import net.kunmc.lab.configlib.ConfigCommandBuilder;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SamplePlugin extends JavaPlugin {
+    public static Config config;
+
+    @Override
+    public void onEnable() {
+        config = new Config(this);
+        config.saveConfigIfAbsent();
+        config.loadConfig();
+
+        ConfigCommand configCommand = new ConfigCommandBuilder(config).build();
+
+        CommandLib.register(this, new TestCommand(configCommand));
+    }
+}
