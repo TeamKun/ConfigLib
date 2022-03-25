5f6bbd2b6b8b54e6965d03f2baf46b72eac20a77
Maru32768 <maru043@docomo.ne.jp>
2022-03-26 04:23:03 +0900

    add Double2DoublePairValue

---
+++ forge/src/main/java/net/kunmc/lab/configlib/value/tuple/Double2DoublePairValue.java
@@ -0,0 +1,50 @@
+package net.kunmc.lab.configlib.value.tuple;
+
+import net.kunmc.lab.commandlib.ArgumentBuilder;
+import net.minecraft.command.CommandSource;
+
+import java.util.List;
+
+public class Double2DoublePairValue extends Double2ObjectPairValue<Double, Double2DoublePairValue> {
+    private transient double min = -Double.MAX_VALUE;
+    private transient double max = Double.MAX_VALUE;
+
+    public Double2DoublePairValue(Double left, Double right) {
+        super(left, right);
+    }
+
+    public Double2DoublePairValue rightMin(double min) {
+        this.min = min;
+        return this;
+    }
+
+    public Double2DoublePairValue rightMax(double max) {
+        this.max = max;
+        return this;
+    }
+
+    @Override
+    protected void appendRightArgument(ArgumentBuilder builder) {
+        builder.doubleArgument("double2");
+    }
+
+    @Override
+    protected boolean isCorrectRightArgument(String entryName, List<Object> argument, CommandSource sender) {
+        return true;
+    }
+
+    @Override
+    protected String incorrectRightArgumentMessage(String entryName, List<Object> argument, CommandSource sender) {
+        return "";
+    }
+
+    @Override
+    protected Double argumentToRightValue(List<Object> argument, CommandSource sender) {
+        return ((Double) argument.get(1));
+    }
+
+    @Override
+    protected String rightToString(Double right) {
+        return right.toString();
+    }
+}
