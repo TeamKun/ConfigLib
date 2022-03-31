1fbbeb571bf266d41275ffe27a11f602d8e4b8f5
Maru32768 <maru32768@gmail.com>
2022-03-31 19:43:12 +0900

    update forge sample

--- sample/forge/src/main/java/net/kunmc/lab/samplemod/SampleMod.java
+++ sample/forge/src/main/java/net/kunmc/lab/samplemod/SampleMod.java
@@ -14,16 +14,13 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 @Mod(SampleMod.MOD_ID)
 public class SampleMod {
     public static final String MOD_ID = "samplemod";
-    public static CommonConfig commonConfig;
-    public static ClientConfig clientConfig;
-    public static ServerConfig serverConfig;
-    public static NotSerializedConfig notSerializedConfig;
+    public static final CommonConfig commonConfig = new CommonConfig(MOD_ID);
+    public static final ClientConfig clientConfig = new ClientConfig(MOD_ID);
+    public static final ServerConfig serverConfig = new ServerConfig(MOD_ID);
+    public static final NotSerializedConfig notSerializedConfig = new NotSerializedConfig(MOD_ID);
     private final ConfigCommandBuilder builder;
 
     public SampleMod() {
-        commonConfig = new CommonConfig(MOD_ID);
-        serverConfig = new ServerConfig(MOD_ID);
-        notSerializedConfig = new NotSerializedConfig(MOD_ID);
         builder = new ConfigCommandBuilder(commonConfig)
                 .addConfig(serverConfig)
                 .addConfig(notSerializedConfig);
@@ -31,10 +28,9 @@ public class SampleMod {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
     public void onServerStartingOnClient(FMLServerStartingEvent event) {
-        clientConfig = new ClientConfig(MOD_ID);
         builder.addConfig(clientConfig);
     }
 
