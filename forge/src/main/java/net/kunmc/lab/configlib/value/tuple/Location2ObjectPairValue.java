e2fef30a7fac2d737160bf8aad92ba6aee62b274
Maru32768 <maru32768@gmail.com>
2022-05-03 10:55:22 +0900

    add Location2ObjectPairValue

---
+++ forge/src/main/java/net/kunmc/lab/configlib/value/tuple/Location2ObjectPairValue.java
@@ -0,0 +1,44 @@
+package net.kunmc.lab.configlib.value.tuple;
+
+import net.kunmc.lab.commandlib.ArgumentBuilder;
+import net.kunmc.lab.commandlib.util.Location;
+import net.minecraft.command.CommandSource;
+
+import java.util.List;
+
+public abstract class Location2ObjectPairValue<R, T extends Location2ObjectPairValue<R, T>> extends PairValue<Location, R, T> {
+    public Location2ObjectPairValue(Location left, R right) {
+        super(left, right);
+    }
+
+    @Override
+    protected void appendLeftArgument(ArgumentBuilder builder) {
+        builder.locationArgument("location");
+    }
+
+    @Override
+    protected boolean isCorrectLeftArgument(String entryName, List<Object> argument, CommandSource sender) {
+        return true;
+    }
+
+    @Override
+    protected String incorrectLeftArgumentMessage(String entryName, List<Object> argument, CommandSource sender) {
+        return "";
+    }
+
+    @Override
+    protected Location argumentToLeftValue(List<Object> argument, CommandSource sender) {
+        return ((Location) argument.get(0));
+    }
+
+    @Override
+    protected String leftToString(Location location) {
+        String worldName = "null";
+        if (location.getWorld() != null) {
+            worldName = location.getWorld().getDimensionKey().getLocation().toString();
+        }
+
+        return String.format("{world=%s,x=%.1f,y=%.1f,z=%.1f,pitch=%.1f,yaw=%.1f}",
+                worldName, location.getX(), location.getY(), location.getZ(), location.getPitch(), location.getYaw());
+    }
+}
