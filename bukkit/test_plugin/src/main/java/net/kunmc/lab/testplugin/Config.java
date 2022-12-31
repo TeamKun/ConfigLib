943f66b8514fd9725caf72b1091246238c27594a
Maru32768 <maru32768@gmail.com>
2023-01-01 00:43:46 +0900

    Bukkit版検証用プラグインプロジェクトを追加

---
+++ bukkit/test_plugin/src/main/java/net/kunmc/lab/testplugin/Config.java
@@ -0,0 +1,14 @@
+package net.kunmc.lab.testplugin;
+
+import net.kunmc.lab.configlib.BaseConfig;
+import net.kunmc.lab.configlib.value.UUIDValue;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class Config extends BaseConfig {
+    public final UUIDValue uuidValue = new UUIDValue();
+
+    public Config(@NotNull Plugin plugin) {
+        super(plugin);
+    }
+}
---
+++ bukkit/test_plugin/src/main/java/net/kunmc/lab/testplugin/TestPlugin.java
@@ -0,0 +1,38 @@
+package net.kunmc.lab.testplugin;
+
+import net.kunmc.lab.commandlib.Command;
+import net.kunmc.lab.commandlib.CommandLib;
+import net.kunmc.lab.configlib.ConfigCommand;
+import net.kunmc.lab.configlib.ConfigCommandBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class TestPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Config config = new Config(this);
+        ConfigCommand configCommand = new ConfigCommandBuilder(config).build();
+
+        CommandLib.register(this, new Command("configlibtest") {{
+            addChildren(configCommand);
+        }});
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public static void print(Object obj) {
+        if (Objects.equals(System.getProperty("plugin.env"), "DEV")) {
+            System.out.printf("[%s] %s%n", TestPlugin.class.getSimpleName(), obj);
+        }
+    }
+
+    public static void broadcast(Object obj) {
+        if (Objects.equals(System.getProperty("plugin.env"), "DEV")) {
+            Bukkit.broadcastMessage(String.format("[%s] %s", TestPlugin.class.getSimpleName(), obj));
+        }
+    }
+}
