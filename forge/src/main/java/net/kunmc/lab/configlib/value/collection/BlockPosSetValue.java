cabd59a00dd4603d5647f7e83feada569b4d085a
Maru32768 <maru32768@gmail.com>
2023-04-04 02:53:06 +0900

    fix(forge): BlockPosSetValueが未実装だった

--- forge/src/main/java/net/kunmc/lab/configlib/value/collection/BlockPosSetValue.java
+++ forge/src/main/java/net/kunmc/lab/configlib/value/collection/BlockPosSetValue.java
@@ -6,6 +6,7 @@ import net.kunmc.lab.commandlib.CommandContext;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -20,46 +21,51 @@ public class BlockPosSetValue extends SetValue<BlockPos, BlockPosSetValue> {
 
     @Override
     protected void appendArgumentForAdd(ArgumentBuilder builder) {
-
-    }
-
-    @Override
-    protected boolean isCorrectArgumentForAdd(String entryName, List<Object> argument, CommandContext ctx) {
-        return false;
-    }
-
-    @Override
-    protected String incorrectArgumentMessageForAdd(String entryName, List<Object> argument, CommandContext ctx) {
-        return null;
+        builder.blockPosArgument("pos");
     }
 
     @Override
     protected Set<BlockPos> argumentToValueForAdd(String entryName, List<Object> argument, CommandContext ctx) {
-        return null;
+        return Collections.singleton((BlockPos) argument.get(0));
     }
 
     @Override
     protected void appendArgumentForRemove(ArgumentBuilder builder) {
-
-    }
-
-    @Override
-    protected boolean isCorrectArgumentForRemove(String entryName, List<Object> argument, CommandContext ctx) {
-        return false;
-    }
-
-    @Override
-    protected String incorrectArgumentMessageForRemove(String entryName, List<Object> argument, CommandContext ctx) {
-        return null;
+        builder.integerArgument("x", sb -> {
+                   value.stream()
+                        .map(BlockPos::getX)
+                        .map(Object::toString)
+                        .forEach(sb::suggest);
+               })
+               .integerArgument("y", sb -> {
+                   double x = ((double) sb.getParsedArgs()
+                                          .get(0));
+                   value.stream()
+                        .filter(l -> l.getX() == x)
+                        .map(BlockPos::getY)
+                        .map(Object::toString)
+                        .forEach(sb::suggest);
+               })
+               .integerArgument("z", sb -> {
+                   double x = ((double) sb.getParsedArgs()
+                                          .get(0));
+                   double y = ((double) sb.getParsedArgs()
+                                          .get(1));
+                   value.stream()
+                        .filter(l -> l.getX() == x && l.getY() == y)
+                        .map(BlockPos::getZ)
+                        .map(Object::toString)
+                        .forEach(sb::suggest);
+               });
     }
 
     @Override
     protected Set<BlockPos> argumentToValueForRemove(String entryName, List<Object> argument, CommandContext ctx) {
-        return null;
+        return Collections.singleton(((BlockPos) argument.get(0)));
     }
 
     @Override
     protected String elementToString(BlockPos blockPos) {
-        return null;
+        return String.format("x=%d,y=%d,z=%d", blockPos.getX(), blockPos.getY(), blockPos.getZ());
     }
 }
